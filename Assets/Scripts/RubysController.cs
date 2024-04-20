5218e5ae1276a97301da6d98f5337c9b20cb2ec0
itapires <itapritsch@gmail.com>
2024-04-20 13:28:23 +0100

    14.Using your code in Unity

--- Assets/Scripts/RubysController.cs
+++ Assets/Scripts/RubysController.cs
@@ -7,7 +7,7 @@ public class RubysController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Vector2 position = transform.position;
+        
     }
 
     // Update is called once per frame
