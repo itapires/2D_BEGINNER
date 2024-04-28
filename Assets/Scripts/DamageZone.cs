ea276c565b6a1f8fae80854cc594d81ffe7f0ece
itapires <itapritsch@gmail.com>
2024-04-28 15:13:22 +0100

    1.Reset Ruby’s Health

--- Assets/Scripts/DamageZone.cs
+++ Assets/Scripts/DamageZone.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthCollectible : MonoBehaviour
+public class DamageZone : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -10,11 +10,8 @@ public class HealthCollectible : MonoBehaviour
 
         if (controller != null)
         {
-            if (controller.health < controller.maxHealth)
-            {
-                controller.ChangeHealth(1);
-                Destroy(gameObject);
-            }
+            controller.ChangeHealth(-1);
         }
     }
+
 }
